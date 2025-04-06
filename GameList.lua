2e8d1bba3362a70f7757db3d15043d7694665b44
IlIIlllllII <yozyyozy251@gmail.com>
2025-04-06 15:18:41 -0500

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [3623096087] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- Muscle Legends
+-- [3101667897] = -- Legends of Speed (SOON)
+}
+
+return Games
