cf39ceea534e40940015f330a8c7d128b97b6a8a
IlIIlllllII <yozyyozy251@gmail.com>
2025-04-06 15:21:19 -0500

    Update and rename Lurnai.lua to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/TryharderKid/LurnaiNewUpdate/refs/heads/main/GameList.lua"))()
+
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+    end
+end
