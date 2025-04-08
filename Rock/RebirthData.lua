3471a6a6eacbf373144a22d4f4de854550247434
IlIIlllllII <yozyyozy251@gmail.com>
2025-04-08 15:06:19 -0500

    Create RebirthData.lua

---
+++ Rock/RebirthData.lua
@@ -0,0 +1,14 @@
+local RebirthData = {}
+
+RebirthData.JungleRock = require("RebirthData.JungleRock")
+RebirthData.MuscleKingRock = require("RebirthData.MuscleKingRock")
+RebirthData.LegendRock = require("RebirthData.LegendRock")
+RebirthData.InfernoRock = require("RebirthData.InfernoRock")
+RebirthData.MysticRock = require("RebirthData.MysticRock")
+RebirthData.FrozenRock = require("RebirthData.FrozenRock")
+RebirthData.GoldenRock = require("RebirthData.GoldenRock")
+RebirthData.LargeRock = require("RebirthData.LargeRock")
+RebirthData.PunchingRock = require("RebirthData.PunchingRock")
+RebirthData.TinyRock = require("RebirthData.TinyRock")
+
+return RebirthData
