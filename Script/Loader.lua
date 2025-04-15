86c84b063c7e456bebe89720c31bb8bb5765e4bc
IlIIlllllII <yozyyozy251@gmail.com>
2025-04-14 23:36:50 -0500

    Update Loader.lua

--- Script/Loader.lua
+++ Script/Loader.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/TryharderKid/LurnaiNewUpdate/refs/heads/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/TryharderKid/LurnaiNewUpdate/refs/heads/main/Script/GameList.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
