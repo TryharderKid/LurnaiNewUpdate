1dfac4fd43f715cec81f7dccd6b4985a467ce1c2
IlIIlllllII <yozyyozy251@gmail.com>
2025-04-14 23:34:57 -0500

    Update GameList.lua

--- Script/GameList.lua
+++ Script/GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [3623096087] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- Muscle Legends
+  [3623096087] = "https://raw.githubusercontent.com/TryharderKid/LurnaiNewUpdate/refs/heads/main/Script/Lurnai.Lua", -- Muscle Legends
 -- [3101667897] = -- Legends of Speed (SOON)
 }
 
