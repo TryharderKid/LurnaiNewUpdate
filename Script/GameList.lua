d0ad05cd72705f045243f6a5d5161da885c1d43d
IlIIlllllII <yozyyozy251@gmail.com>
2025-04-15 16:53:21 -0500

    Update GameList.lua

--- Script/GameList.lua
+++ Script/GameList.lua
@@ -1,6 +1,9 @@
 local Games = {
   [3623096087] = "https://raw.githubusercontent.com/TryharderKid/LurnaiNewUpdate/refs/heads/main/Script/Lurnai.Lua", -- Muscle Legends
--- [3101667897] = -- Legends of Speed (SOON)
+  [3101667897] = "NoUiYet", -- [3101667897] = -- Legends of Speed (SOON)
+  [2753915549] = "NoUiYet", -- Blox Fruits (First Sea)
+  [4442272183] = "NoUiYet", -- Blox Fruits (Second Sea)
+  [7449423635] = "NoUiYet", -- Blox Fruits (Third Sea)
 }
 
 return Games
